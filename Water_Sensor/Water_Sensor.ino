c0f7f39833614b0d38d2176d3aec3acea3a315ba
kmcclenn <kmcclennen@jhcschool.org>
2022-03-22 11:36:05 -0600

    added comments and cleaned up code

--- Water_Sensor/Water_Sensor.ino
+++ Water_Sensor/Water_Sensor.ino
@@ -13,30 +13,29 @@ int setupTime = 100; // first 100 repetitions of loop
 
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(motorPin, OUTPUT);
 }
 
 void loop() {
   
-  int avgValueA = getMoisture(moistureSensorA, pastReadingsA, initial_counter)[0];
-  pastReadingsA = getMoisture(moistureSensorA, pastReadingsA, initial_counter)[1];
+  int avgValueA = getMoisture(moistureSensorA, pastReadingsA, initial_counter)[0]; // average value of past 100 sensings for moisture sensor A
+  pastReadingsA = getMoisture(moistureSensorA, pastReadingsA, initial_counter)[1]; // array of past 100 moisture readings
   initial_counter++;
-  
+
+  // first check if the soil is dry
   if (avgValueA > threshold && !drynessA) {
     drynessA = true;
-    long int start_timeA = millis();
+    long int start_timeA = millis(); // starts the start time
   }
 
-//  avgValue = getMoisture(moistureSensor, pastReadings)[0]
-//  pastReadings = getMoisture(moistureSensor, pastReadings)[1]
-//  initial_counter++;
+  // waters plants
   water(drynessA, start_timeA, avgValueA);
   
   
 }
 
+// average function
 int average(int array_to_be_averaged[]) {
   long int sum = 0;
   int len = sizeof(array_to_be_averaged) / sizeof(array_to_be_averaged[0]);
@@ -48,28 +47,35 @@ int average(int array_to_be_averaged[]) {
   return average;
 }
 
+// get moisture function that returns the average value of the past 100 readings and the array of the past 100 readings
 int * getMoisture(int sensorPin, int previousReadings[], int initial_counter) {
   int moistureValue = analogRead(sensorPin);
+
+  // for first 100 iterations, fill in array. After that, make array the 100 most recent readings.
   if (initial_counter < setupTime) {
     previousReadings[initial_counter] = moistureValue;
   } else {
-    
     for (int i = 0; i < 99; i++) {
       pastReadings[i] = pastReadings[i+1];
     }
     pastReadings[99] = moistureValue;
   }
+
+  // take average and map
   int avgValue = average(pastReadings);
   avgValue = map(avgValue, 200, 700, 0, 1000);
   avgValue = min(avgValue, 1000);
   avgValue = max(avgValue, 0);
   Serial.println(avgValue);
-  //delay(1000);
+  
   return [avgValue, previousReadings];
 }
 
+//water function
 void water(boolean dryness, int start_time, int avgValue) {
+  // if the soil is dry
   if (dryness && initial_counter > setupTime) {
+    // if the soil is still dry after the delay time, then water it. if it is wet again, then don't water and repeat
     if (millis() - start_time > delayTimeAfterWatering && avgValue >= threshold) {
       digitalWrite(motorPin, LOW);
       delay(waterTime);
@@ -84,7 +90,8 @@ void water(boolean dryness, int start_time, int avgValue) {
   }
 }
 
-// previous code to get moisture in loop()
+/////////// previous code to get moisture in loop() /////////
+
 //int moistureValue = analogRead(moistureSensor);
 //  if (initial_counter < 100) {
 //    pastReadingsA[initial_counter] = moistureValue;
